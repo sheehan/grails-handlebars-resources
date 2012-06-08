9814453c72b516e01389bc8528bd112cba367d5a
Matt Sheehan <matt.sheehan@objectpartners.com>
2012-06-07 22:36:34 -0500

    update documentation

--- HandlebarsResourcesGrailsPlugin.groovy
+++ HandlebarsResourcesGrailsPlugin.groovy
@@ -14,7 +14,7 @@ This plug-in supports using Handlebars.js templates with the Grails Resources Pl
 the template files into JavaScript and make it available for other resource processing, including minification and bundling.
 The Handlebars.js resource files are also included.
 '''
-    def documentation = "http://grails.org/plugin/handlebars-resources"
+    def documentation = "https://github.com/sheehan/handlebars-resources/blob/master/README.md"
     def license = "APACHE"
     def issueManagement = [ system: "github", url: "https://github.com/sheehan/handlebars-resources/issues" ]
     def scm = [ url: "https://github.com/sheehan/handlebars-resources" ]
