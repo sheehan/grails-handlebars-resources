176b5ac3d29d6c3b941a55bf9b9d97ae35582437
David Moles <chronodm@gmail.com>
2013-01-16 10:12:50 -0800

    Fixed extra quotation marks

--- grails-app/conf/HandlebarsResources.groovy
+++ grails-app/conf/HandlebarsResources.groovy
@@ -1,9 +1,9 @@
 modules = {
     handlebars {
-        resource id: 'handlebars', 'url:[plugin: 'handlebars-resources', dir: 'js', file: 'handlebars-1.0.rc.1.js']
+        resource id: 'handlebars', url:[plugin: 'handlebars-resources', dir: 'js', file: 'handlebars-1.0.rc.1.js']
     }
 
     handlebars_runtime {
-        resource id: 'handlebars_runtime', 'url:[plugin: 'handlebars-resources', dir: 'js', file: 'handlebars.runtime-1.0.rc.1.js']
+        resource id: 'handlebars_runtime', url:[plugin: 'handlebars-resources', dir: 'js', file: 'handlebars.runtime-1.0.rc.1.js']
     }
 }
