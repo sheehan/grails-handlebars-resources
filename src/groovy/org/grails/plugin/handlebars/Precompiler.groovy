d7e9a5f1c7fde46c699d6133989aec8df2e57c08
trashhalo <stephen@mindjunk.org>
2013-04-03 10:37:33 -0400

    Added the configuration option to set known helpers during precompilation.

--- src/groovy/org/grails/plugin/handlebars/Precompiler.groovy
+++ src/groovy/org/grails/plugin/handlebars/Precompiler.groovy
@@ -1,17 +1,20 @@
 package org.grails.plugin.handlebars
 
-import org.mozilla.javascript.Scriptable
-import org.mozilla.javascript.Function
-import org.mozilla.javascript.Context
-import org.mozilla.javascript.tools.shell.Global
-
 import javax.annotation.PostConstruct
+
+import org.mozilla.javascript.Context
+import org.mozilla.javascript.Function
+import org.mozilla.javascript.NativeObject
+import org.mozilla.javascript.Scriptable
+import org.mozilla.javascript.tools.shell.Global
 
 class Precompiler {
 
     private Scriptable scope
     private Function precompile
-
+	private NativeObject options
+	List<String> extraKnownHelpers
+	
     @PostConstruct
     void init() {
         ClassLoader classLoader = getClass().classLoader
@@ -24,17 +27,25 @@ class Precompiler {
         scope = cx.initStandardObjects(global)
         cx.evaluateString scope, handlebars.text, handlebars.file, 1, null
 
-        precompile = scope.get("Handlebars", scope).get("precompile", scope)
-        Context.exit();
+		precompile = scope.get("Handlebars", scope).get("precompile", scope)
+		options = new NativeObject()
+		if(extraKnownHelpers){
+			NativeObject extra = new NativeObject()
+			extraKnownHelpers.each{
+				extra.defineProperty(it, true, NativeObject.READONLY)
+			}
+			options.defineProperty("knownHelpers", extra, NativeObject.READONLY)
+		}
+		Context.exit();
     }
 
     void precompile(File input, File target, String templateName) {
-        String compiledTemplate = callPrecompile(input.text)
+		String compiledTemplate = callPrecompile(input.text,options)
         target.write wrapTemplate(templateName, compiledTemplate)
     }
 
     private synchronized String callPrecompile(Object[] args) {
-        Context.call null, precompile, scope, scope, args
+		Context.call null, precompile, scope, scope, args
     }
 
     def wrapTemplate = { String templateName, String compiledTemplate ->
